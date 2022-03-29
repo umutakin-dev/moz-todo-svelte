398b9e3ecc81f8f12bc99393b18bba6d41edbe20
umut akin <umutakin@fastmail.com>
2022-03-29 10:16:38 +0300

    02 - dynamically generating the to-dos from the data

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -48,86 +48,32 @@
 
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    <!-- todo-1 (editing mode) -->
-    <li class="todo">
-      <div class="stack-small">
-        <form class="stack-small">
-          <div class="form-group">
-            <label for="todo-1" class="todo-label">
-              New name for 'Create a Svelte starter app'
-            </label>
+    {#each todos as todo (todo.id)}
+      <li class="todo">
+        <div class="stack-small">
+          <div class="c-cb">
             <input
-              type="text"
-              id="todo-1"
-              autocomplete="off"
-              class="todo-text"
+              type="checkbox"
+              id="todo-{todo.id}"
+              checked={todo.completed}
             />
+            <label for="todo-{todo.id}" class="todo-label">
+              {todo.name}
+            </label>
           </div>
           <div class="btn-group">
-            <button class="btn todo-cancel" type="button">
-              Cancel
-              <span class="visually-hidden"
-                >renaming Create a Svelte starter app</span
-              >
+            <button type="button" class="btn">
+              Edit <span class="visually-hidden">{todo.name}</span>
             </button>
-            <button class="btn btn__primary todo-edit" type="submit">
-              Save
-              <span class="visually-hidden"
-                >new name for Create a Svelte starter app</span
-              >
+            <button type="button" class="btn btn__danger">
+              Delete <span class="visually-hidden">{todo.name}</span>
             </button>
           </div>
-        </form>
-      </div>
-    </li>
-
-    <!-- todo-2 -->
-    <li class="todo">
-      <div class="stack-small">
-        <div class="c-cb">
-          <input type="checkbox" id="todo-2" checked />
-          <label for="todo-2" class="todo-label">
-            Create your first component
-          </label>
         </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden">Create your first component</span>
-          </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden">Create your first component</span>
-          </button>
-        </div>
-      </div>
-    </li>
-
-    <!-- todo-3 -->
-    <li class="todo">
-      <div class="stack-small">
-        <div class="c-cb">
-          <input type="checkbox" id="todo-3" />
-          <label for="todo-3" class="todo-label">
-            Complete the rest of the tutorial
-          </label>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden"
-              >Complete the rest of the tutorial</span
-            >
-          </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden"
-              >Complete the rest of the tutorial</span
-            >
-          </button>
-        </div>
-      </div>
-    </li>
+      </li>
+    {:else}
+      <li>Nothing to do here!</li>
+    {/each}
   </ul>
 
   <hr />
