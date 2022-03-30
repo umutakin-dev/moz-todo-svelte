02340837c76ca9d921754d10a9bd3a8022661ec1
umut akin <umutakin@fastmail.com>
2022-03-30 10:04:04 +0300

    04 - other actions improvements

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -8,6 +8,7 @@
   let nameEl;
 
   let editing = false;
+  let editButtonPressed = false;
   let name = todo.name;
 
   function update(updatedTodo) {
@@ -29,11 +30,12 @@
     dispatch("remove", todo);
   }
 
-  async function onEdit() {
-    editing = true;
-    await tick();
-    nameEl.focus();
+  function onEdit() {
+    editButtonPressed = true; // user pressed the Edit button, focus will come back to the Edit button
+    editing = true; // enter editing mode
   }
+
+  const focusEditButton = (node) => editButtonPressed && node.focus();
 
   function onToggle() {
     update({completed: !todo.completed});
@@ -87,7 +89,7 @@
       <label for="todo-{todo.id}" class="todo-label">{todo.name}</label>
     </div>
     <div class="btn-group">
-      <button type="button" class="btn" on:click={onEdit}>
+      <button type="button" class="btn" on:click={onEdit} use:focusEditButton>
         Edit<span class="visually-hidden"> {todo.name}</span>
       </button>
       <button type="button" class="btn btn__danger" on:click={onRemove}>
